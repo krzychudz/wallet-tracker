f96a3855f27822a996c8d4e67703a6d2dd4e382d
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2021-10-18 20:56:24 +0200

    Add layout for add account page

--- lib/screens/account/add_account.dart
+++ lib/screens/account/add_account.dart
@@ -5,6 +5,33 @@ class AddAccountScreen extends StatelessWidget {
 
   const AddAccountScreen({Key? key}) : super(key: key);
 
+  Widget _buildAccountNameForm() {
+    return TextField(
+      decoration: const InputDecoration(
+        border: UnderlineInputBorder(),
+        labelText: 'Nazwa konta',
+      ),
+    );
+  }
+
+  Widget _buildAccountInfoForm() {
+    return TextField(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Informacje o koncie',
+      ),
+      maxLines: 2,
+      maxLength: 50,
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return ElevatedButton(
+      child: Text("Dodaj konto"),
+      onPressed: () => {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,11 +42,30 @@ class AddAccountScreen extends StatelessWidget {
           style: Theme.of(context).textTheme.headline1,
         ),
         iconTheme: IconThemeData(
-          color: Colors.black, //change your color here
+          color: Colors.black,
         ),
       ),
-      body: Container(
-        child: Text("Add Account"),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 16.0,
+          bottom: 16.0,
+          left: 16.0,
+          right: 100.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildAccountNameForm(),
+            SizedBox(
+              height: 16,
+            ),
+            _buildAccountInfoForm(),
+            SizedBox(
+              height: 16,
+            ),
+            _buildSubmitButton()
+          ],
+        ),
       ),
     );
   }
